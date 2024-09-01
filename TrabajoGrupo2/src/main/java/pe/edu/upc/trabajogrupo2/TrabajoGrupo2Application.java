346fabedfc3b048bd8b7f95a9c12da20f979ba9f
Sebastian <103966774+SebCordova@users.noreply.github.com>
2024-09-01 10:08:51 -0500

    Add files via upload

---
+++ TrabajoGrupo2/src/main/java/pe/edu/upc/trabajogrupo2/TrabajoGrupo2Application.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.trabajogrupo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoGrupo2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrabajoGrupo2Application.class, args);
+    }
+
+}
---
+++ TrabajoGrupo2/src/test/java/pe/edu/upc/trabajogrupo2/TrabajoGrupo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.trabajogrupo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabajoGrupo2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
